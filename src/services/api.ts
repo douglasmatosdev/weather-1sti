5ad8ae8753098ed21dc53440c0f8b197335a83b5
douglasmatosdev <douglasmatosdev@gmail.com>
2023-05-13 16:57:55 -0300

    add types

--- src/services/api.ts
+++ src/services/api.ts
@@ -18,18 +18,18 @@ const api = {
         return fetch(`${url.byId}${id}&${AUTH}&${BR_SETUP}`)
             .then(res => res && res.json())
     },
-    // getByGroupId: (groupId: number[]): Promise<ResponseConsultGroupForecast> => {
-    //     return fetch(`${url.byGroupId}${groupId}&${AUTH}&${BR_SETUP}`)
-    //         .then(res => res && res.json())
-    // },
-    // getByName: (name: string): Promise<ResponseConsult> => {
-    //     return fetch(`${url.byName}${name}&${AUTH}&${BR_SETUP}`)
-    //         .then(res => res && res.json())
-    // },
-    // getForecastByName: (name: string): Promise<ResponseConsultForecast> => {
-    //     return fetch(`${url.forecastByName}${name}&${AUTH}&${BR_SETUP}`)
-    //         .then(res => res && res.json())
-    // }
+    getByGroupId: (groupId: number[]): Promise<ResponseConsultGroupForecast> => {
+        return fetch(`${url.byGroupId}${groupId}&${AUTH}&${BR_SETUP}`)
+            .then(res => res && res.json())
+    },
+    getByName: (name: string): Promise<ResponseConsult> => {
+        return fetch(`${url.byName}${name}&${AUTH}&${BR_SETUP}`)
+            .then(res => res && res.json())
+    },
+    getForecastByName: (name: string): Promise<ResponseConsultForecast> => {
+        return fetch(`${url.forecastByName}${name}&${AUTH}&${BR_SETUP}`)
+            .then(res => res && res.json())
+    }
 }
 
 export default api
--- src/types/weather-api.d.ts
+++ src/types/weather-api.d.ts
@@ -13,3 +13,80 @@ declare type ResponseConsult = {
     weather: Weather
     wind: Wind
 }
+
+
+declare type Clouds = {
+    all: number
+}
+
+declare type Coord = {
+    lat: number
+    lon: number
+}
+
+declare type Main = {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+}
+
+declare type Sys = {
+    country: string
+    id: number
+    sunrise: number
+    sunset: number
+    type: number
+}
+
+declare type Weather = {
+    description: string
+    icon: string
+    id: number
+    main: string
+}[]
+
+declare type Wind = {
+    deg: number
+    speed: number
+}
+
+type City = {
+    coord: Coord
+    country: string
+    id: number
+    name: string
+    population: number
+    sunrise: number
+    sunset: number
+    timezone: number
+}
+
+
+declare type ResponseConsultGroupForecast = {
+    cnt: number
+    list: ListForecast[]
+}
+
+declare type ListForecast = {
+    clouds: Clouds
+    dt: number
+    dt_txt: string
+    main: Main & { grnd_level: number; sea_level: number; temp_kf: number; }
+    pop: number
+    name: string
+    sys: { prod: number }
+    visibility: number
+    weather: Weather
+    wind: Wind & { gust: number }
+}
+
+declare type ResponseConsultForecast = {
+    city: City
+    cnt: number
+    cod: string
+    list: ListForecast[]
+    massage: 0
+}
