2bdfc5ea00cbb86232edd38fb6e9749ec19503c8
douglasmatosdev <douglasmatosdev@gmail.com>
2023-05-19 15:44:43 -0300

    api authentication

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXT_PUBLIC_TOKEN: 'ff2fecb8105897817a0f45b712c70259'
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,7 @@
-// const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
-// const API_KEY = 'a66d13253dc39685dfaabd202f467c33'//process.env.NEXT_PUBLIC_TOKEN
-// const AUTH = `appid=${API_KEY}`
 const BR_SETUP = 'units=metric&lang=pt_br'
-//http://api.openweathermap.org/data/2.5/weather?q=duque%20de%20caxias&appid=a66d13253dc39685dfaabd202f467c33&units=metric
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
-// const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='
-
-const API_KEY = 'db04f7f0492b01563ff78567693c5b79'
+const API_KEY = process.env.NEXT_PUBLIC_TOKEN
 const AUTH = `&appid=${API_KEY}&${BR_SETUP}`
-
 
 const url = {
     byId: `${BASE_URL}weather?id=`,
