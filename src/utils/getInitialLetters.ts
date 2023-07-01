69c2fdd7316b6812cc775fb12329b451198f80ed
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-01 12:30:12 -0300

    remove letter

--- src/utils/getInitialLetters.ts
+++ src/utils/getInitialLetters.ts
@@ -1,5 +1,5 @@
 export const getInitialLetters = (value: string): string => {
     return value.split(' ').reduce((acc, curr) => {
         return acc += curr.charAt(0).toUpperCase()
-    }, '')
+    }, '').slice(1)
 }
