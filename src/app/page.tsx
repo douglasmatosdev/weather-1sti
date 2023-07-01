4332af9aab377c134b2739c3eb9852c299195f3d
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-01 12:36:28 -0300

    fix title in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,13 +24,15 @@ export default function Home() {
 
   return (
     <main className="w-full flex flex-col mx-auto p-0 m-0">
-      <div className="w-full max-w-screen-sm mx-auto flex flex-col items-center md:px-6 py-8 justify-center border-b border-[#FFE7C7]">
-        <h1 className={`inline-block text-white ml-6 md:ml-0 md:mb-4 whitespace-wrap text-${displayCard ? 2 : 6}xl sm:text-6xl whitespace-wrap font-bold leading-[64px]`}>
-          Previsão do tempo
-        </h1>
+      <div className="w-full max-w-screen-sm mx-auto flex flex-col items-center md:px-6 pb-8 justify-center border-b border-[#FFE7C7]">
+        <div className='w-full p-2 flex items-center'>
+          <h1 className={`inline-block text-white ml-6 md:ml-0 md:mb-4 whitespace-wrap text-${displayCard ? 2 : 6}xl sm:text-6xl whitespace-wrap font-bold leading-[64px]`}>
+            Previsão do tempo
+          </h1>
+        </div>
 
         {
-          displayCard && data? <CardWeather handler={setDisplayCard} data={mountCardValues(data)} /> : null
+          displayCard && data ? <CardWeather handler={setDisplayCard} data={mountCardValues(data)} /> : null
         }
         <div className="sm:w-full w-[90%] max-w-xl mt-4 md:mt-12flex justify-center">
           <InputText
